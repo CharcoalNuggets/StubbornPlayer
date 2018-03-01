ead2ce51d8565439241d0e3e6371e041b00b84c8
CharcoalNuggets <dgin@mail.csuchico.edu>
2018-02-28 17:21:53 -0800

    A game where you have no free will

--- main.cpp
+++ main.cpp
@@ -4,8 +4,25 @@
 using namespace std;
 
 int main()
-{
+{ 
   SecretDoor game;
+  int response;
+  int wins=0;
+  
+  cout<< "How many times would you like to play?\n";
+  cin>> response;
+
+  for ( int gameCount = 0; gameCount < response; gameCount++ )
+  {
+  game.newGame();
+  game.guessDoorC();
+  game.guessDoorC();
+   if ( game.isWinner() )
+   {
+   wins++;
+   }
+  }
+  cout<< "You won " << wins << " times.\n";
     
   return 0;
 }
